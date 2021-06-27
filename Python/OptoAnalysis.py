c6092ac5c521b68d2e6623239debe1e78409a3a8
palme876 <palme876@umn.edu>
2021-06-26 22:36:14 -0500

    add session metadata to opto_pymedpc.m

--- Python/OptoAnalysis.py
+++ Python/OptoAnalysis.py
@@ -23,68 +23,33 @@ if __name__ == '__main__':
     #setting squeeze_me argument=true, otherwise each element seems to get nested in individual arrays
     matContents= sio.loadmat(r'Z:\Dakota\MEDPC\Downstairs\vp-vta-stgtacr_DStrain\Opto DS Task Test- CUE Laser Manipulation\trainData.mat',squeeze_me=True)
     
-    #%% kaggle 
-    #https://www.kaggle.com/avilesmarcel/open-mat-in-python-pandas-dataframe
-    mdata= matContents['trainData'] #struct variable from .mat 
-    # mdata= matContents['trainData'][0]
-    #looks like we have 109 np arrays, one for each struct element (session)
+    #%% Extract the relevant data and get the data into pandas.dataframe format
+    #adapted from https://www.kaggle.com/avilesmarcel/open-mat-in-python-pandas-dataframe
     
-    mtype= mdata.dtype 
-    #looks like there are 17 'types' of data here, corresponding to each column
+    mdata= matContents['trainData'] #extract the struct contents from the loaded .mat 
     
-    #now it looks like we are creating a dict matching up data from mdata with column names
-    # ndata= {n: mdata[n][0,0]for n in mtype.names} #seems to be dict matching column titles with nparray of data
-    #i think above works for 1 element struct, but I think need below to get all sessions
-    ndata= {n: mdata[n] for n in mtype.names} #seems to be dict matching column titles with nparray of data
+    mtype= mdata.dtype  #1 'type' of data corresponding to each column
+    
+    #now it looks like we are creating a dict matching up data (in np.arrays) from mdata with column names
+    ndata= {n: mdata[n] for n in mtype.names} 
     
 
     data_headline= [] #will hold list of var labels for columns
-    data_raw=[]
-    # data_raw= np.empty([mdata.shape[1],len(mtype.names)],dtype=object) #not squeezed
-    data_raw= np.empty([mdata.shape[0],len(mtype.names)],dtype=object) # squeezed
-
-    #since our struct contains multiple data types, make this np array of object type
-    # data_raw[:]=np.NaN
+    data_raw=[]   #will hold the raw data for each column
+    #since our struct contains multiple data types/classes, make this np array of object type
+    data_raw= np.empty([mdata.shape[0],len(mtype.names)],dtype=object) 
     
+    
+    #now simply loop through each variable type and get the corresponding data. Probably a better way to do this
     for var in range(len(mtype.names)):   
-    
     
         data_headline.append([mtype.names[var]][0])
     
-        # data_raw.append(mdata[data_headline[var]]) #grab data matching this var name header
-        for ses in range(data_raw.shape[0]): #probably inefficient looping through each row but couldn't get assignment to work without it
+        for ses in range(data_raw.shape[0]): 
                 data_raw[ses,var]= ndata[mtype.names[var]].flatten()[ses]
-
-                #try converting to single data type for comparison
-                # data_raw[ses,var]= np.asarray(data_raw[ses,var]).astype(float)
-                #Remove string type data
-                # if type(data_raw[ses,var][0])==numpy.str_
-                # data_raw[ses,var]
-
+                
     #save the data as a pandas dataframe 
     df= pd.DataFrame(data_raw, columns=data_headline)
-    
-    #Something is keeping us from running ProfileReport()
-    #TypeError: Cannot compare types 'ndarray(dtype=object)' and 'float'
-    
-    #I'm thinking it's just that I have multiple data points as arrays in each element of the dataframe
-    #instead of running on the raw data would be good to try with some calculated variables of interest e.g. PE ratio
-    
-    #might be shape of ndarrays?
-    # df[data_headline[5]][0].shape
-    # type(df[data_headline[5]][0])
-    # df[data_headline[5]][0].flatten().shape
-    # type(df[data_headline[5]][0].flatten()) #still ndarray after flattening
-    
-    #seems that ProfileReport doesn't like the ndarrays within df
-    #so try converting to numerical
-    # for var in range(df.shape[1]):
-    #     if (type(df[data_headline[var]][0])).__name__=='string':
-    #         print(var)
-    #         # df[data_headline[var]].apply(pd.to_numeric)
-    #         df[data_headline[var]]= df[data_headline[var]].astype('object')
-    #         df[data_headline[var]]= pd.to_numeric(df[data_headline[var]], errors='coerce')
-    
     
     #%% May need to do some reaarranging of data for easiest plotting
     #https://stackoverflow.com/questions/38372016/split-nested-array-values-from-pandas-dataframe-cell-over-multiple-rows?newreg=1c04242af3c7428faaecc1daf25d783a
@@ -92,155 +57,162 @@ if __name__ == '__main__':
     #e.g. could have one row for each date and 60 columns for trial series?
     
     #let's just get a few variables
-df2= df[['RatID','x_M_DSPElatency','x_E_DSLaser','x_N_NSPElatency','x_F_NSLaser']].copy()
-
-
-#really interesting potential solution looks like here we get 2 indices, one for subject and one for file (date)
-unnested_lst = []
-for col in df2.columns:
-    unnested_lst.append(df2[col].apply(pd.Series).stack())
-result = pd.concat(unnested_lst, axis=1, keys=df2.columns).fillna(method='ffill')
-
-#Now let's perform some preliminary analysis, make a new variable for DS PE outcome per trial
-result['DSPEoutcome']= result['x_M_DSPElatency']
-result['DSPEoutcome']=result['DSPEoutcome'].replace(10,0) #10s= no PE
-result.loc[result['DSPEoutcome'] > 0, 'DSPEoutcome'] = 1
-#also replace 10s latencies with nan (since there was no PE)
-result.x_M_DSPElatency[result.x_M_DSPElatency==10] = np.nan
-
-#same for NS
-result['NSPEoutcome']= result['x_N_NSPElatency']
-result['NSPEoutcome']=result['NSPEoutcome'].replace(10,0) #10s= no PE
-result.loc[result['NSPEoutcome'] > 0, 'NSPEoutcome'] = 1
-#also replace 10s latencies with nan (since there was no PE)
-result.x_N_NSPElatency[result.x_N_NSPElatency==10] = np.nan
-    #visualize
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-#This works!!
-# sns.relplot(x=result['file'], y=result['x_K_PEtimestamps'],hue=result['RatID'], data= result)
-
-# sns.histplot(x='x_E_DSLaser',y='x_M_DSPElatency',hue='RatID', data=result)
-
-sns.relplot(x='RatID',y='x_M_DSPElatency',hue='x_E_DSLaser',data=result)
-
-plt.figure()
-plt.subplot(1,2,1) #DS
-sns.violinplot(x='RatID',y='x_M_DSPElatency',hue='x_E_DSLaser',data=result)
-plt.subplot(1,2,2) #NS
-sns.violinplot(x='RatID',y='x_N_NSPElatency',hue='x_F_NSLaser',data=result)
-
-plt.figure()
-plt.subplot(1,2,1) #DS
-sns.barplot(x='RatID',y='x_M_DSPElatency',hue='x_E_DSLaser',data=result)
-
-
-
-plt.figure()
-sns.boxplot(x='RatID',y='x_M_DSPElatency',hue='x_E_DSLaser',data=result)
-
-# #try multiple hist()
-plt.figure()
-sns.histplot(data=result, x='x_M_DSPElatency', hue='x_E_DSLaser', multiple='layer')
-
-# g = sns.FacetGrid(result, hue='x_E_DSLaser', size=7.5)
- 
-# g.map(sns.distplot, 'x_M_DSPELatency', bins=10,
-#       kde=False, rug=True).add_legend()
- 
-# g.set(xlabel='DSPELatency',
-#       ylabel='Frequency',
-#       title='Distribution of DS PElat by laser state')
-
-# plt.figure()
-# #plot DS PE latency based on Laser state?
-# grid = sns.FacetGrid(result,row='Group_0_Stgtacr_1_Mcherry_',col='Sex')
-# #this works but I think am only getting one session?
-# grid.map(lambda _x,_y,**kwargs : sns.histplot(x=_x.values,hue=_y.values),'x_M_DSPElatency','x_E_DSLaser')
-
-
-#%% Extract Date from filenames
-# #just converting filename to date
-# import re, datetime
-
-# for ses in range(len(df['file'])):
-#     match = re.search('\d{4}\d{2}\d{2}', df['file'][ses][0])
-#     df['file'][ses] = datetime.datetime.strptime(match.group(), '%Y%m%d').date()
-
-    
-
-#%% Help from stackexchange
-#import plotting resources
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-# Apply the default theme
-sns.set_theme() 
-
-#create dataframe df2 using example data
-df2= df[['RatID','file','x_K_PEtimestamps']].copy()
-
-#%% try relplot, fails- unhashable type: 'numpy.ndarray'
-# sns.relplot(x=df2['file'],y=df2['x_K_PEtimestamps'],hue=df2['RatID'], data=df2)
-   
-#%% try scatter plot
-# sns.scatterplot(x=df2['file'],y=df2['x_K_PEtimestamps'])
-
-#%% try collect datat in loop, then scatter?
-# x= np.empty(len(df2),dtype= 'object')
-# y= np.empty(len(df2),dtype= 'object')
-# for ses in range(len(df2)):
-#     x[ses]= df2['file'][ses]
-#     y[ses]= df2['x_K_PEtimestamps'][ses]
-#     yStack[ses]= y[ses].column_stack()
-    
-# # yStack= y.column_stack()
-    
-# sns.scatterplot(x=x[ses],y=y)
-
-#%% Doing some basic visualizations
-import seaborn as sns
-import matplotlib.pyplot as plt
-# Apply the default theme
-sns.set_theme()    
-
-#plot relationships between some variables
-# for ses in range(len(df)):
-# sns.relplot(x=df['file'][0],y=df['x_O_DSPE'][0].flatten(), hue=df['RatID'][0])
-
-# for ses in range(len(df)):
-    # df['file'][ses]= df['file'][ses][0]
-    # df['subject'][ses]= df['subject'][ses][0]
-    # df['Group_0_Stgtacr_1_Mcherry_'][ses]= df['Group_0_Stgtacr_1_Mcherry_'][ses][0][0]
-
-    
-df['file']= df['file'].astype('category')
-df['subject']= df['subject'].astype('category')
-df['Mode_0_stgtacr_1_mCherry_']= df['Group_0_Stgtacr_1_Mcherry_'].astype('category')
-
-#don't fully understand what is being done here... but it is plotting without reorganizing data
-#plot DS PE latency based on Laser state
-grid = sns.FacetGrid(df,row='Group_0_Stgtacr_1_Mcherry_',col='Sex')
-grid.map(lambda _x,_y,**kwargs : plt.scatter(_x.values[0],_y.values[0]),'x_E_DSLaser','x_M_DSPElatency')
-
-#plot DS PE latency based on Laser state
-grid = sns.FacetGrid(df,row='Group_0_Stgtacr_1_Mcherry_',col='Sex')
-#this works but I think am only getting one session?
-grid.map(lambda _x,_y,**kwargs : sns.histplot(x=_x.values[0],hue=_y.values[0]),'x_M_DSPElatency','x_E_DSLaser')
-
-# #try visualizing DS PE and latency based on Laser state
-# grid = sns.FacetGrid(df,row='x_E_DSLaser',col='Group_0_Stgtacr_1_Mcherry') 
-# grid.map(lambda _x,_y,**kwargs : plt.scatter(_x.values[0],_y.values[0]),'x_O_DSPE','x_M_DSPElatency')
+    df2= df[['RatID','x_M_DSPElatency','x_E_DSLaser','x_N_NSPElatency','x_F_NSLaser']].copy()
     
     
-#%% Try Pandas_profiling report
-from pandas_profiling import ProfileReport
+    #really interesting potential solution looks like here we get 2 indices, one for subject and one for file (date)
+    #but, I think different sized variables within session complicates this factor and causes many repeating values that aren't accurate?
+    unnested_lst = []
+    for col in df2.columns:
+        unnested_lst.append(df2[col].apply(pd.Series).stack())
+    result = pd.concat(unnested_lst, axis=1, keys=df2.columns).fillna(method='ffill')
+    
+    #Now let's perform some preliminary analysis, make a new variable for DS PE outcome per trial
+    result['DSPEoutcome']= result['x_M_DSPElatency']
+    result['DSPEoutcome']=result['DSPEoutcome'].replace(10,0) #10s= no PE
+    result.loc[result['DSPEoutcome'] > 0, 'DSPEoutcome'] = 1
+    #also replace 10s or 0s latencies with nan (since there was no PE)
+    #not sure where the 0s latencies are coming from, but are there for every trial 30 & 31?
+    result.x_M_DSPElatency[result.x_M_DSPElatency==10] = np.nan
+    result.x_M_DSPElatency[result.x_M_DSPElatency==0]= np.nan
+    
+    #same for NS
+    result['NSPEoutcome']= result['x_N_NSPElatency']
+    result['NSPEoutcome']=result['NSPEoutcome'].replace(10,0) #10s= no PE
+    result.loc[result['NSPEoutcome'] > 0, 'NSPEoutcome'] = 1
+    #also replace 10s latencies with nan (since there was no PE)
+    result.x_N_NSPElatency[result.x_N_NSPElatency==10] = np.nan
+    result.x_N_NSPElatency[result.x_N_NSPElatency==0]= np.nan
 
-#infinite error?
-# profile = ProfileReport(df, title='Pandas Profiling Report') #, explorative = True)
+        #visualize
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    
+    #This works!!
+    # sns.relplot(x=result['file'], y=result['x_K_PEtimestamps'],hue=result['RatID'], data= result)
+    
+    # sns.histplot(x='x_E_DSLaser',y='x_M_DSPElatency',hue='RatID', data=result)
+    
+    sns.relplot(x='RatID',y='x_M_DSPElatency',hue='x_E_DSLaser',data=result)
     
     
+    
+    plt.figure()
+    # plt.subplot(1,2,1) #DS #violin too dense by subj
+    # sns.violinplot(x='RatID',y='x_M_DSPElatency',hue='x_E_DSLaser',data=result, split=True, inner='stick')
+    sns.violinplot(x='RatID',y='x_M_DSPElatency',hue='x_E_DSLaser',data=result, cut=0, split=True, inner='quartile') #, scale='count')
+
+    plt.figure() 
+    # plt.subplot(1,2,2) #NS #violin too dense by subj
+    sns.violinplot(x='RatID',y='x_N_NSPElatency',hue='x_F_NSLaser',data=result, cut=0, split=True, inner='quartile') #, scale='count')
+    
+    
+    #~~note that seaborn documentation says that ci=68 can be used to plot SEM but this is actually different from sem. Default is 95% CI i think
+    #ci 68 assumes normal distro and then it's still only 68% probability that mean lies here?
+    plt.figure()
+    plt.subplot(1,2,1) #DS 
+    sns.barplot(x='RatID',y='x_M_DSPElatency',hue='x_E_DSLaser',data=result,capsize=.2) 
+    plt.subplot(1,2,2) #NS
+    sns.barplot(x='RatID',y='x_N_NSPElatency',hue='x_F_NSLaser',data=result, capsize=.2)
+    
+    
+    plt.figure()
+    plt.subplot(1,2,1) #DS
+    sns.boxplot(x='RatID',y='x_M_DSPElatency',hue='x_E_DSLaser',data=result)
+    # sns.swarmplot(x='RatID',y='x_M_DSPElatency',hue='x_E_DSLaser',data=result, dodge=True,size=1.5,color='.2')
+    plt.subplot(1,2,2) #NS
+    sns.boxplot(x='RatID',y='x_N_NSPElatency',hue='x_F_NSLaser',data=result)
+    # sns.swarmplot(x='RatID',y='x_N_NSPElatency',hue='x_F_NSLaser',data=result, dodge=True,size=1.5,color='.2')
+
+
+    # #try multiple hist()
+    #initially, hist() by count makes it seem that NS + laser has higher latency, but after normalization here looks same
+    plt.figure()
+    plt.subplot(1,2,1) #DS
+    sns.histplot(data=result, x='x_M_DSPElatency', hue='x_E_DSLaser',  stat="density", common_norm=False, kde=True, multiple='layer',bins=20)
+    plt.subplot(1,2,2) #NS
+    sns.histplot(data=result, x='x_N_NSPElatency', hue='x_F_NSLaser',  stat="density", common_norm=False, kde=True, multiple='layer',bins=20)
+    
+    #ecdf plot
+    plt.figure()
+    plt.subplot(1,2,1) #DS
+    sns.ecdfplot(data=result, x='x_M_DSPElatency', hue='x_E_DSLaser')
+    plt.subplot(1,2,2) #NS
+    sns.ecdfplot(data=result, x='x_N_NSPElatency', hue='x_F_NSLaser')
+
+    #%% Trying to melt() each event variable into eventType and eventTime 
+    #goal here is to reduce arrays of event timestamps into single element
+    #could have hierarchical indexing for date & subject
+    
+    # df3= df.melt(id_vars='x_C_DSlaserState')
+
+    #related?https://stackoverflow.com/questions/53218931/how-to-unnest-explode-a-column-in-a-pandas-dataframe 
+    def unnesting(df, explode):
+        idx = df.index.repeat(df[explode[0]].str.len())
+        df1 = pd.concat([
+            pd.DataFrame({x: np.concatenate(df[x].values)}) for x in explode], axis=1)
+        df1.index = idx
+        return df1.join(df.drop(explode, 1), how='left')
+
+    
+    df3= unnesting(df,['x_E_DSLaser'])
+    
+    #     Option 2
+    
+    # If the sublists have different length, you need an additional step:
+    
+    vals = df.B.values.tolist()
+    rs = [len(r) for r in vals]    
+    a = np.repeat(df.A, rs)
+    
+    pd.DataFrame(np.column_stack((a, np.concatenate(vals))), columns=df.columns)
+        
+    #%% Doing some more basic visualizations
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    # Apply the default theme
+    sns.set_theme()    
+    
+    #plot relationships between some variables
+    # for ses in range(len(df)):
+    # sns.relplot(x=df['file'][0],y=df['x_O_DSPE'][0].flatten(), hue=df['RatID'][0])
+    
+    # for ses in range(len(df)):
+        # df['file'][ses]= df['file'][ses][0]
+        # df['subject'][ses]= df['subject'][ses][0]
+        # df['Group_0_Stgtacr_1_Mcherry_'][ses]= df['Group_0_Stgtacr_1_Mcherry_'][ses][0][0]
+    
+        
+    df['file']= df['file'].astype('category')
+    df['subject']= df['subject'].astype('category')
+    df['Mode_0_stgtacr_1_mCherry_']= df['Group_0_Stgtacr_1_Mcherry_'].astype('category')
+    
+    #don't fully understand what is being done here... but it is plotting without reorganizing data
+    #plot DS PE latency based on Laser state
+    grid = sns.FacetGrid(df,row='Group_0_Stgtacr_1_Mcherry_',col='Sex')
+    grid.map(lambda _x,_y,**kwargs : plt.scatter(_x.values[0],_y.values[0]),'x_E_DSLaser','x_M_DSPElatency')
+    
+    #plot DS PE latency based on Laser state
+    grid = sns.FacetGrid(df,row='Group_0_Stgtacr_1_Mcherry_',col='Sex')
+    #this works but I think am only getting one session?
+    grid.map(lambda _x,_y,**kwargs : sns.histplot(x=_x.values[0],hue=_y.values[0]),'x_M_DSPElatency','x_E_DSLaser')
+    
+    # #try visualizing DS PE and latency based on Laser state
+    # grid = sns.FacetGrid(df,row='x_E_DSLaser',col='Group_0_Stgtacr_1_Mcherry') 
+    # grid.map(lambda _x,_y,**kwargs : plt.scatter(_x.values[0],_y.values[0]),'x_O_DSPE','x_M_DSPElatency')
+        
+        
+    #%% Try Pandas_profiling report
+    #note- if you are getting errors with ProfileReport() and you installed using conda, remove and reinstall using pip install  
+    from pandas_profiling import ProfileReport
+    
+    profile = ProfileReport(result, title='Pandas Profiling Report', explorative = True)
+        
+    #%% save profile report as html
+    profile.to_file('pandasProfile.html')
+
+    
+        
     
      #%% result is a numpy structured array, let's convert it to pandas dataframe
     # # df= pd.DataFrame(matContents['trainData'], index=[0]) #.set_index('f0')
